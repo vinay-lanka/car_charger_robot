5a0303ec625778e90a4fdd463f8c22545a986457
Vinay Lanka <vinaylanka111@gmail.com>
2023-11-30 01:27:03 -0500

    first commit

---
+++ launch/car_world.launch.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import PythonExpression, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import SetEnvironmentVariable
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.event_handlers import OnProcessExit
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    world = os.path.join(
+        get_package_share_directory('crx_description'),
+        'worlds/car_world.world'
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+    path_to_urdf = get_package_share_directory('crx_description') + '/urdf/' + 'crx_description.urdf.xacro'
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'robot_description': ParameterValue(
+                Command(['xacro ', str(path_to_urdf)]), value_type=str
+            )
+        }]
+    )
+    # start_joint_state_publisher_cmd = Node(
+    #     package='joint_state_publisher',
+    #     executable='joint_state_publisher',
+    #     name='joint_state_publisher'
+    # )
+
+    start_gazebo_ros_spawner_cmd = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-entity', 'crx_description', 
+                    '-topic', 'robot_description',
+                    '-x', x_pose,
+                    '-y', y_pose,
+                    '-z', '0.0'],
+        output='screen'
+    )
+
+    # Joint State Broadcaster Node
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    # Joint Position Controller Node
+    robot_position_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["position_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_postion_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_position_controller_spawner],
+        )
+    )
+
+    ####### 3D Camera Spawning
+    path_to_camera_urdf = get_package_share_directory('crx_description') + '/urdf/' + 'camera_urdf.urdf.xacro'
+    camera_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        namespace='test',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'robot_description': ParameterValue(
+                Command(['xacro ', str(path_to_camera_urdf)]), value_type=str
+            )
+        }]
+    )
+    camera_gazebo_ros_spawner_cmd = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-entity', 'camera_urdf', 
+                    '-topic', '/test/robot_description',
+                    '-x', '-1.0',
+                    '-y', '-1.0',
+                    '-z', '0.0'],
+        output='screen'
+    )
+
+    ld = LaunchDescription(
+        []
+    )
+
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_node)
+    # ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(start_gazebo_ros_spawner_cmd)
+    ld.add_action(joint_state_broadcaster_spawner)
+    ld.add_action(delay_robot_postion_controller_spawner_after_joint_state_broadcaster_spawner)
+    ld.add_action(camera_state_publisher_node)
+    ld.add_action(camera_gazebo_ros_spawner_cmd)
+    return ld
